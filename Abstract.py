8779b18da656d8a57dc0aef88ad33113431df066
YOGESH B <91959419+yogeshb2002@users.noreply.github.com>
2021-12-03 21:17:44 +0530

    Add files via upload
    Usage the of Abstract class and method with simple example.

---
+++ Abstract.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod #importing the method, class to use abstract class
+class Programer1:
+    
+    @abstractmethod     #abstract method
+    def coding(self):
+        pass
+
+class Programer2(Programer1):
+
+    def coding(self):                   #defing the method
+        print("Writing the code...")
+
+Arun = Programer2()
+Arun.coding()
